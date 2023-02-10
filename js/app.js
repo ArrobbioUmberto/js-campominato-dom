29584d2365266c4e0aa3dc8a5e44fc256fb1aea9
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-10 18:25:44 +0100

    FEAT level setting

--- js/app.js
+++ js/app.js
@@ -1,40 +1,46 @@
 console.log('ciao')
 
 
-// const numeroCelle = 10
-// console.log(numeroCelle)
-// const tabellaArea = numeroCelle**2 
-// console.log(tabellaArea)
+const numeroCelle = 10
+console.log(numeroCelle)
+const tabellaArea = numeroCelle**2 
+console.log(tabellaArea)
 
-// const appendHTML = document.querySelector('.row')
+const appendHTML = document.querySelector('.row')
+const playHTML = document.querySelector('.play')
+playHTML.innerHTML= `<select id="select" name="level" style="width: 100px" class="d-flex flex-row" >
+                         <option value="first">Easy</option>
+                         <option value="middle">Medium</option>
+                         <option value="strong">Hard</option>
+                    </select>`
+const btnElement = document.querySelector('.btn')
+console.log(btnElement)
 
-// const btnElement = document.querySelector('.btn')
-// console.log(btnElement)
-// btnElement.addEventListener('click', function(){
-//     appendHTML.innerHTML = ''
-//     for (let i = 0; i < tabellaArea; i++){
-//         console.log(i+1)
-//         const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella fs-2" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
-//         appendHTML.innerHTML += createDiv    
-//     }
-//     console.log(appendHTML)
-//     const celleElements = document.querySelectorAll('.cella')
-//       console.log(celleElements)
+btnElement.addEventListener('click', function(){
+    appendHTML.innerHTML = ''
+    for (let i = 0; i < tabellaArea; i++){
+        console.log(i+1)
+        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella fs-2" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
+        appendHTML.innerHTML += createDiv    
+    }
+    console.log(appendHTML)
+    const celleElements = document.querySelectorAll('.cella')
+      console.log(celleElements)
 
-//     for (let i = 0; i < celleElements.length; i++) {
-// 	const cella = celleElements[i]
+    for (let i = 0; i < celleElements.length; i++) {
+	const cella = celleElements[i]
 
-// 	cella.addEventListener('click', function () {
-//         cella.classList.add('color')
-// 		console.log(i + 1)
-// 	})
-// }
-// })
+	cella.addEventListener('click', function () {
+        cella.classList.add('color')
+		console.log(i + 1)
+	})
+}
+})
 
 
 
 // Fase due esercizio 
-let quanteBombe = 8
+let quanteBombe = 16
 let  numeriElencoBomba = []
 do{
     for (let i=0; i < quanteBombe; i++){
