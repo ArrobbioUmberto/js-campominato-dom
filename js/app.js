f9aed99cf71babf24b2a28fde98e93a79ac0d95a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-11 11:43:41 +0100

    FEAT color red for bomb number

--- js/app.js
+++ js/app.js
@@ -31,19 +31,47 @@ btnElement.addEventListener('click', function () {
     const tabellaArea = numeroCelle ** 2
     console.log(tabellaArea)
     appendHTML.innerHTML = ''
-    for (let i = 0; i < tabellaArea; i++) {
-        console.log(i + 1)
-        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella fs-2" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i + 1}</div>`
+    for (let i = 1; i < tabellaArea + 1; i++) {
+        // console.log(i + 1)
+        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella fs-2" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i}</div>`
         appendHTML.innerHTML += createDiv
     }
     console.log(appendHTML)
+    // fino a qui abbiamo generato la tabella richiesta dall'utente 
+
+    // adesso generiamo la lista con le bombe e la loro posizione 
+    let quanteBombe = 16
+    let numeriElencoBomba = []
+    do {
+        for (let i = 1; i < quanteBombe + 1; i++) {
+            // genero un numero randomico tra i valori della tabella 
+            let numeriBomba = parseInt((Math.random() * (100, 1)) * tabellaArea)
+            // console.log(numeriBomba)
+            if (!numeriElencoBomba.includes(numeriBomba)) {
+                numeriElencoBomba.push(numeriBomba)
+                // console.log(numeriElencoBomba)
+            } else {
+                quanteBombe += 1
+            }
+        }
+
+    } while (numeriElencoBomba.lenght < quanteBombe)
+    console.log(numeriElencoBomba)
+
+    // adesso dobbiamo collegare la lista delle bombe alle varie celle per collegarle ai colori rosso o verde 
+
+
     const celleElements = document.querySelectorAll('.cella')
     console.log(celleElements)
     for (let i = 0; i < celleElements.length; i++) {
         const cella = celleElements[i]
-
+        const counter = i + 1
         cella.addEventListener('click', function () {
-            cella.classList.add('color')
+            if (numeriElencoBomba.includes(counter)){
+                cella.classList.add('color-bomb')
+            } else {
+                cella.classList.add('color')
+            }
             console.log(i + 1)
         })
     }
